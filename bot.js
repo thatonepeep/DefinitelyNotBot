6ead945590e1f691826f6fd500a00f9ad4ff16f3
thatonepeep <37483157+thatonepeep@users.noreply.github.com>
2018-03-21 19:04:56 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const args = message.content.slice(prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -15,6 +17,17 @@ client.on('message', message => {
     if (message.content === 'bing') {
     	message.reply('BONG!');
   	}
+ bot.on("message", function(message) {
+    if (message.author.equals(bot.user)) return;
+
+    if (!message.content.startswith(PREFIX)) return;
+
+var args = messahe.content.substring(PREFIX.LENGTH).split(" ");
+ 
+    switch (args[0].toLowerCase()) {
+     case "Info";
+      message.channel.sendMessage("Property of MistyPeak.Bot made by ThatOnePeep#6581,it was designed to help members of the MistyPeak's community to know their way around the commands and the server/group/website. Type /help for detailed help with such things as commands and general questions."))
+ 
 });
 
 // THIS  MUST  BE  THIS  WAY
